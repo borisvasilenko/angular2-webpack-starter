da2ac7966279e120dd00f6b22837960225b30329
Boris Vasilenko <vasilenko@dreaktor.com>
2016-06-18 10:37:49 +0300

    suggested fix

--- src/polyfills.browser.ts
+++ src/polyfills.browser.ts
@@ -7,7 +7,17 @@
 // import 'es7-reflect-metadata';
 
 // Prefer CoreJS over the polyfills above
-import 'core-js/es6';
+
+// Commented out the whole es6.
+//  NOTE: More correct solution is to exclude only one polyfill, which causes the issue: core-js/es6/promise.
+// import "core-js/es6";
+
+// Added parts of es6 which are necessary for your project or your browser support requirements.
+import "core-js/es6/array";
+import "core-js/es6/object";
+// (the rest is omitted)
+import "core-js/es6/string";
+
 import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
 
